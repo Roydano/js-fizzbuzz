7491e83ca0113fdda5a2d01672cc8503ee6fbbba
Roydano <roy.dano90@gmail.com>
2021-06-03 13:05:30 +0200

    init esercizio + jsnack fatt in classe

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// JSnack5
+
+//Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+// Per chi vuole possiamo fare che finchè l'utente non inserisce il numero, il programma non va avanti.
+
+
+
+
+
+
+
+//JSnack6
+
+// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di invitati.
+// p.s potete scegliere se farne 4/5 oppure generarli all'infinito fino a quando non vi stufate.
+
+
+
+
+// JSnack7
+
+// Consegna:
+// Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi anche "Fizz" di fianco al numero e per i multipli di 5 stampi "Buzz". Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz". :collisione:
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Come faccio a sapere se un numero è divisibile per?
+// Abbiamo visto qualcosa di particolare che possiamo usare?
+// Consigli del giorno:
+// scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
